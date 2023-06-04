a0a1146f915e92e8a20acbd286813c0a217ee8ff
scooter7 <scooter.vineburgh@gmail.com>
2023-06-04 09:27:10 -0500

    Update c4.py

--- c4.py
+++ c4.py
@@ -36,7 +36,7 @@ placeholders = {
 def generate_article(content_type, keyword, writing_style, audience, style_guide, institution, word_count):
     
     messages = [
-        {"role": "user", "content": "This will be a " + content_type},
+        {"role": "user", "content": "This will be a SEO optimized " + content_type},
         {"role": "user", "content": "This will be " + content_type + " about " + keyword},
         {"role": "user", "content": "The " + content_type + " should have the style " + writing_style},
         {"role": "user", "content": "The email should mention the benefits of attending " + institution},
