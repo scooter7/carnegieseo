4bb9cb5d12934d7ef04ddc1338f27d5a4282217d
scooter7 <scooter.vineburgh@gmail.com>
2023-06-03 23:19:53 -0500

    Update ccc2.py

--- ccc2.py
+++ ccc2.py
@@ -24,41 +24,34 @@ def generate_article(keyword, writing_style, institution, word_count):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": f"Write a SEO optimized word {article_type} about " + keyword},
-            {"role": "user", "content": "The {article_type} should be " + writing_style},
-            {"role": "user", "content": "The {article_type} should mention the benefits of attending " + institution},
-            {"role": "user", "content": "The {article_type} length should " + str(word_count)},
-        ]
+                {"role": "user", "content": "Write an email about " + keyword},
+                {"role": "user", "content": "The email should be " + writing_style},
+                {"role": "user", "content": "The email should mention the benefits of attending " + institution},
+                {"role": "user", "content": "The email length should " + str(word_count)},
+            ]
     )
-    result = ""
+    result = ''
     for choice in response.choices:
         result += choice.message.content
 
     print(result)
     return result
 
-article_type = st.selectbox("Select content type:", ["Article", "Email"])
 keyword = st.text_input("Enter a keyword:")
 writing_style = st.selectbox("Select writing style:", ["Casual", "Informative", "Witty"])
 institution = st.text_input("Institution:")
 word_count = st.slider("Select word count:", min_value=300, max_value=1000, step=100, value=300)
-submit_button = st.button("Generate Content")
+submit_button = st.button("Generate Email")
 
 if submit_button:
     message = st.empty()
     message.text("Busy generating...")
-    
-    if article_type == "Article":
-        message_type = "SEO optimized word article"
-    else:
-        message_type = "email"
-    
-    article = generate_article_type(keyword, writing_style, institution, word_count, article_type=message_type)
+    article = generate_article(keyword, writing_style, institution, word_count)
     message.text("")
     st.write(article)
     st.download_button(
-        label="Download content",
+        label="Download email",
         data=article,
-        file_name='Content.txt',
+        file_name= 'Email.txt',
         mime='text/txt',
     )
