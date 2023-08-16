94cfb2aaa425c9c46fd44e7238f7e529e09700e8
scooter7 <scooter.vineburgh@gmail.com>
2023-08-16 11:46:08 -0500

    Update c14.py

--- c14.py
+++ c14.py
@@ -25,39 +25,42 @@ placeholders = {
     "Beige - dedicated, humble": {"verbs": ["dedicate", "humble"], "adjectives": ["dedicated", "humble"]}
 }
 
+style_weights = [0.2, 0.3, 0.1, 0.2, 0.2]
+
 def generate_article(content, writing_styles):
     messages = [{"role": "system", "content": "You are a content creator that changes the tone of user-generated content based on the writing styles listed."}]
     messages.append({"role": "user", "content": content})
-    for style in writing_styles:
-        messages.append({"role": "assistant", "content": f"The content should have {style} style."})
+    for i, style in enumerate(writing_styles):
+        weight = style_weights[i]
+        messages.append({"role": "assistant", "content": f"The content should have {style} style with a weight of {weight * 100:.1f}%."})
     messages.append({"role": "assistant", "content": "Generating the content..."})
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
     return response.choices[0].message["content"]
 
-def main():
-    user_content = st.text_area("Paste your content here:")
-    writing_styles = st.multiselect("Select Writing Styles:", list(placeholders.keys()))
-    
-    if st.button("Generate Revised Content"):
-        revised_content = generate_article(user_content, writing_styles)
-        st.text(revised_content)
-        st.download_button("Download Revised Content", revised_content, "revised_content.txt")
+user_content = st.text_area("Paste your content here:")
+writing_styles = st.multiselect("Select Writing Styles:", list(placeholders.keys()))
 
-    st.markdown("---")
-    st.header("Revision Section")
-    
-    pasted_content = st.text_area("Paste Generated Content Here (for further revisions):")
-    revision_requests = st.text_area("Specify Revisions Here:")
+if st.button("Generate Revised Content"):
+    revised_content = generate_article(user_content, writing_styles)
+    st.text(revised_content)
+    st.download_button("Download Revised Content", revised_content, "revised_content.txt")
 
-    if st.button("Revise"):
-        revision_messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": pasted_content},
-            {"role": "user", "content": revision_requests}
-        ]
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=revision_messages)
-        revised_content = response.choices[0].message["content"]
-        st.text(revised_content)
+st.markdown("---")
+st.header("Revision Section")
+
+pasted_content = st.text_area("Paste Generated Content Here (for further revisions):")
+revision_requests = st.text_area("Specify Revisions Here:")
+
+if st.button("Revise"):
+    revision_messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": pasted_content},
+        {"role": "user", "content": revision_requests}
+    ]
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=revision_messages)
+    revised_content = response.choices[0].message["content"]
+    st.text(revised_content)
+
 
 if __name__ == "__main__":
     main()
