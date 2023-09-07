5aaeed79ad084d0ffc3b8370b6060c260a66ce04
scooter7 <scooter.vineburgh@gmail.com>
2023-09-07 17:21:36 -0500

    Update contentaudit.py

--- contentaudit.py
+++ contentaudit.py
@@ -41,7 +41,7 @@ def generate_pdf(fig, top_colors, examples, text):
     pdf.set_font("Arial", size=12)
     
     pdf.image("chart.png", x=10, y=pdf.get_y(), w=190)
-    pdf.ln(65)
+    pdf.ln(165)
     
     for color in top_colors:
         pdf.cell(200, 10, f"Top Color: {color}", ln=1)
