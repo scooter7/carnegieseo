7cec2cc104db4a7f7579e15694f88771d50ceea5
scooter7 <scooter.vineburgh@gmail.com>
2023-09-10 01:19:01 -0500

    Update cca.py

--- cca.py
+++ cca.py
@@ -8,9 +8,7 @@ import plotly.graph_objects as go
 import openai
 
 # Define GPT-3 API key
-OPENAI_API_KEY = "OPENAI_API_KEY"
-
-# Rest of your code remains the same...
+OPENAI_API_KEY = "YOUR_OPENAI_API_KEY"
 
 def analyze_with_gpt3(text, prompt):
     openai.api_key = OPENAI_API_KEY
@@ -54,48 +52,82 @@ def extract_examples(text, color_keywords, top_colors):
         examples[color] = list(examples[color])[:3]
     return examples
 
-# Modify the main() function
+def parse_tone_scores(response_text):
+    # Implement the function to parse tone scores from the GPT-3 response
+    pass
+
+def draw_quadrant_chart(tone_scores, title, x_labels, y_labels):
+    # Implement the function to create a quadrant chart
+    pass
+
+def generate_word_doc(top_colors, examples, user_content, general_analysis, tone_scores, new_tone_scores):
+    # Implement the function to generate a Word document with analysis
+    pass
+
 def main():
     st.title('Color Personality Analysis')
-    if 'OPENAI_API_KEY' not in st.secrets:
-        st.error('Please set the OPENAI_API_KEY secret on the Streamlit dashboard.')
+    
+    if not OPENAI_API_KEY:
+        st.error('Please set the OPENAI_API_KEY variable with your OpenAI API key.')
         return
-    openai_api_key = st.secrets['OPENAI_API_KEY']
-    color_keywords = {...}
+
+    color_keywords = {
+        'Red': ['Activate', 'Animate', 'Amuse', 'Captivate', 'Cheer', 'Delight', 'Encourage', 'Energize', 'Engage', 'Enjoy', 'Enliven', 'Entertain', 'Excite', 'Express', 'Inspire', 'Joke', 'Motivate', 'Play', 'Stir', 'Uplift', 'Amusing', 'Clever', 'Comedic', 'Dynamic', 'Energetic', 'Engaging', 'Enjoyable', 'Entertaining', 'Enthusiastic', 'Exciting', 'Expressive', 'Extroverted', 'Fun', 'Humorous', 'Interesting', 'Lively', 'Motivational', 'Passionate', 'Playful', 'Spirited'],
+        'Silver': ['Activate', 'Campaign', 'Challenge', 'Commit', 'Confront', 'Dare', 'Defy', 'Disrupting', 'Drive', 'Excite', 'Face', 'Ignite', 'Incite', 'Influence', 'Inspire', 'Inspirit', 'Motivate', 'Move', 'Push', 'Rebel', 'Reimagine', 'Revolutionize', 'Rise', 'Spark', 'Stir', 'Fight', 'Free', 'Aggressive', 'Bold', 'Brazen', 'Committed', 'Courageous', 'Daring', 'Disruptive', 'Driven', 'Fearless', 'Free', 'Gutsy', 'Independent', 'Inspired', 'Motivated', 'Rebellious', 'Revolutionary', 'Unafraid', 'Unconventional'],
+        'Blue': ['Accomplish', 'Achieve', 'Affect', 'Assert', 'Cause', 'Command', 'Determine', 'Direct', 'Dominate', 'Drive', 'Empower', 'Establish', 'Guide', 'Impact', 'Impress', 'Influence', 'Inspire', 'Lead', 'Outpace', 'Outshine', 'Realize', 'Shape', 'Succeed', 'Transform', 'Win', 'Accomplished', 'Assertive', 'Authoritative', 'Commanding', 'Confident', 'Decisive', 'Distinguished', 'Dominant', 'Elite', 'Eminent', 'Established', 'Exceptional', 'Expert', 'First-class', 'First-rate', 'Impressive', 'Influential', 'Leading', 'Magnetic', 'Managerial', 'Masterful', 'Noble', 'Premier', 'Prestigious', 'Prominent', 'Proud', 'Strong'],
+        'Yellow': ['Accelerate', 'Advance', 'Change', 'Conceive', 'Create', 'Engineer', 'Envision', 'Experiment', 'Dream', 'Ignite', 'Illuminate', 'Imagine', 'Innovate', 'Inspire', 'Invent', 'Pioneer', 'Progress', 'Shape', 'Spark', 'Solve', 'Transform', 'Unleash', 'Unlock', 'Advanced', 'Brilliant', 'Conceptual', 'Enterprising', 'Expert', 'Extraordinary', 'Forward-looking', 'Forward-thinking', 'Fresh', 'Future-minded', 'Future-thinking', 'Ingenious', 'Intelligent', 'Inventive', 'Leading-edge', 'Luminous', 'New', 'Pioneering', 'Reforming', 'Rising', 'Transformative', 'Visionary', 'World-changing', 'World-class'],
+        'Green': ['Analyze', 'Discover', 'Examine', 'Expand', 'Explore', 'Extend', 'Inquire', 'Journey', 'Launch', 'Move', 'Pioneer', 'Pursue', 'Question', 'Reach', 'Search', 'Uncover', 'Venture', 'Wonder', 'Adventurous', 'Analytical', 'Curious', 'Discerning', 'Experiential', 'Exploratory', 'Fearless', 'Inquisitive', 'Intriguing', 'Investigative', 'Journeying', 'Mysterious', 'Philosophical', 'Pioneering', 'Questioning', 'Unbound', 'Unexpected'],
+        'Purple': ['Accommodate', 'Assist', 'Befriend', 'Care', 'Collaborate', 'Connect', 'Embrace', 'Empower', 'Encourage', 'Foster', 'Give', 'Help', 'Nourish', 'Nurture', 'Promote', 'Protect', 'Provide', 'Serve', 'Share', 'Shepherd', 'Steward', 'Tend', 'Uplift', 'Value', 'Welcome', 'Affectionate', 'Admirable', 'Age-less', 'Beautiful', 'Classic', 'Desirable', 'Detailed', 'Dreamy', 'Elegant', 'Enchanting', 'Enriching', 'Ethereal', 'Excellent', 'Exceptional', 'Experiential', 'Exquisite', 'Glamorous', 'Graceful', 'Idealistic', 'Inspiring', 'Lofty', 'Mysterious', 'Ordered', 'Perfect', 'Poised', 'Polished', 'Pristine', 'Pure', 'Refined', 'Romantic', 'Sophisticated', 'Spiritual', 'Timeless', 'Traditional', 'Virtuous', 'Visionary']
+    }
+    
     user_content = st.text_area('Paste your content here:')
+    
     if st.button('Analyze'):
         color_counts = analyze_text(user_content, color_keywords)
         total_counts = sum(color_counts.values())
+        
         if total_counts == 0:
             st.write('No relevant keywords found.')
             return
+        
         sorted_colors = sorted(color_counts.items(), key=lambda x: x[1], reverse=True)
         top_colors = [color for color, _ in sorted_colors[:3]]
         labels = [k for k, v in color_counts.items() if v > 0]
         sizes = [v for v in color_counts.values() if v > 0]
+        
         fig = draw_donut_chart(labels, sizes)
         st.plotly_chart(fig)
+        
         examples = extract_examples(user_content, color_keywords, top_colors)
         for color in top_colors:
             st.write(f'Examples for {color}:')
             st.write(', '.join(examples[color]))
-        try:
-            tone_analysis_prompt = """Based on the following text, please provide scores between 1 to 10 for the following traits: - relaxed: calm, laid-back, stress-free - assertive: confident, self-assured - introverted: reserved, quiet - extroverted: outgoing, social"""
-            tone_scores = analyze_with_gpt3(user_content, openai_api_key, tone_analysis_prompt)
-            additional_tone_prompt = """Based on the following text, please provide scores between 1 to 10 for the following additional traits: - conservative: traditional, resistant to change - progressive: open to new ideas, forward-thinking - emotive: expressing emotion - informative: providing useful or interesting information"""
-            new_tone_scores = analyze_with_gpt3(user_content, openai_api_key, additional_tone_prompt)
-            combined_tone_scores = {**tone_scores, **new_tone_scores}
-            general_analysis = 'Your text was analyzed by GPT-3 to determine the following traits based on your tone: ' + ', '.join([f"{k}: {v}" for k, v in combined_tone_scores.items()])
-            st.write('GPT-3 Analysis:')
-            st.write(general_analysis)
+        
+        tone_analysis_prompt = 'Assess the text for tone based on the definitions: relaxed (calm, laid-back), assertive (confident, self-assured), introverted (reserved, solitary), and extroverted (sociable, outgoing). Provide scores from 1 to 10 for each trait.'
+        tone_analysis_response = analyze_with_gpt3(user_content, tone_analysis_prompt)
+        
+        if tone_analysis_response:
+            tone_scores = parse_tone_scores(tone_analysis_response)
+            st.write('Tone Analysis:')
+            st.write(', '.join(f'{k}: {v}' for k, v in tone_scores.items()))
+            
             fig1 = draw_quadrant_chart(tone_scores, 'Tone Quadrant Chart', ['Relaxed', 'Assertive'], ['Extroverted', 'Introverted'])
-            fig2 = draw_quadrant_chart(new_tone_scores, 'Additional Tone Quadrant Chart', ['Conservative', 'Progressive'], ['Emotive', 'Informative'])
             st.plotly_chart(fig1)
+        
+        additional_tone_prompt = 'Assess the text for additional tone based on the definitions: conservative (traditional, resistant to change), progressive (forward-thinking, open to change), emotive (expressing emotion), and informative (providing information). Provide scores from 1 to 10 for each trait.'
+        additional_tone_response = analyze_with_gpt3(user_content, additional_tone_prompt)
+        
+        if additional_tone_response:
+            new_tone_scores = parse_tone_scores(additional_tone_response)
+            st.write('Additional Tone Analysis:')
+            st.write(', '.join(f'{k}: {v}' for k, v in new_tone_scores.items()))
+            
+            fig2 = draw_quadrant_chart(new_tone_scores, 'Additional Tone Quadrant Chart', ['Conservative', 'Progressive'], ['Emotive', 'Informative'])
             st.plotly_chart(fig2)
-            word_file_path = generate_word_doc(top_colors, examples, user_content, general_analysis, tone_scores, new_tone_scores)
-            download_file(word_file_path)
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+        
+        general_analysis = 'Your text was analyzed by GPT-3 to determine the following traits based on your tone: ' + ', '.join([f"{k}: {v}" for k, v in {**tone_scores, **new_tone_scores}.items()])
+        word_file_path = generate_word_doc(top_colors, examples, user_content, general_analysis, tone_scores, new_tone_scores)
+        download_file(word_file_path)
 
 if __name__ == '__main__':
     main()
