00ea321ae3a78c05ef1e9293d0ed275a1383f7a8
scooter7 <scooter.vineburgh@gmail.com>
2023-09-11 17:10:19 -0500

    Update ccauditor.py

--- ccauditor.py
+++ ccauditor.py
@@ -88,10 +88,6 @@ def main():
         st.error("Please set the OPENAI_API_KEY secret on the Streamlit dashboard.")
         return
     openai_api_key = st.secrets["OPENAI_API_KEY"]
-    if 'sentence_to_colors' not in st.session_state:
-        st.session_state.sentence_to_colors = {}
-    if 'tone_scores' not in st.session_state:
-        st.session_state.tone_scores = {}
     color_keywords = {
         'Red': ['Activate', 'Animate', 'Amuse', 'Captivate', 'Cheer', 'Delight', 'Encourage', 'Energize', 'Engage', 'Enjoy', 'Enliven', 'Entertain', 'Excite', 'Express', 'Inspire', 'Joke', 'Motivate', 'Play', 'Stir', 'Uplift', 'Amusing', 'Clever', 'Comedic', 'Dynamic', 'Energetic', 'Engaging', 'Enjoyable', 'Entertaining', 'Enthusiastic', 'Exciting', 'Expressive', 'Extroverted', 'Fun', 'Humorous', 'Interesting', 'Lively', 'Motivational', 'Passionate', 'Playful', 'Spirited'],
         'Silver': ['Activate', 'Campaign', 'Challenge', 'Commit', 'Confront', 'Dare', 'Defy', 'Disrupting', 'Drive', 'Excite', 'Face', 'Ignite', 'Incite', 'Influence', 'Inspire', 'Inspirit', 'Motivate', 'Move', 'Push', 'Rebel', 'Reimagine', 'Revolutionize', 'Rise', 'Spark', 'Stir', 'Fight', 'Free', 'Aggressive', 'Bold', 'Brazen', 'Committed', 'Courageous', 'Daring', 'Disruptive', 'Driven', 'Fearless', 'Free', 'Gutsy', 'Independent', 'Inspired', 'Motivated', 'Rebellious', 'Revolutionary', 'Unafraid', 'Unconventional'],
@@ -108,48 +104,48 @@ def main():
     tone_fig = None
     updated_fig = None
     if st.button('Analyze'):
+        initial_color_counts = Counter()
         sentences = re.split(r'[.!?]', user_content)
         sentence_to_colors = {}
-        updated_color_counts = Counter()
         for sentence in sentences:
             if not sentence.strip():
                 continue
-            sentence_colors = analyze_text(sentence, color_keywords)
-            selected_colors = [color for color, count in sentence_colors.items() if count > 0]
-            sentence_to_colors[sentence] = selected_colors
-            for color in selected_colors:
-                updated_color_counts[color] += 1
-        st.session_state.sentence_to_colors = sentence_to_colors
-        st.session_state.updated_color_counts = updated_color_counts
-        initial_fig = draw_donut_chart(updated_color_counts)
+            initial_colors = []
+            for color, keywords in color_keywords.items():
+                if any(keyword.lower() in sentence.lower() for keyword in keywords):
+                    initial_colors.append(color)
+                    initial_color_counts[color] += 1
+            sentence_to_colors[sentence] = initial_colors
+        initial_fig = draw_donut_chart(initial_color_counts)
         st.subheader('Initial Donut Chart')
         st.plotly_chart(initial_fig)
-        st.session_state.tone_scores = analyze_tone_with_gpt3(user_content, openai_api_key)
-        tone_fig = go.Figure(data=[go.Bar(x=list(st.session_state.tone_scores.keys()), y=list(st.session_state.tone_scores.values()))])
+        tone_scores = analyze_tone_with_gpt3(user_content, openai_api_key)
+        tone_fig = go.Figure(data=[go.Bar(x=list(tone_scores.keys()), y=list(tone_scores.values()))])
         tone_fig.update_layout(xaxis_title='Tone', yaxis_title='Level')
         st.subheader("Tone Analysis")
         st.plotly_chart(tone_fig)
-    if 'tone_scores' in st.session_state and st.session_state.tone_scores:
+        st.session_state.sentence_to_colors = sentence_to_colors
+        st.session_state.tone_scores = tone_scores
+    if 'tone_scores' in st.session_state:
         for tone in st.session_state.tone_scores.keys():
-            st.session_state.tone_scores[tone] = st.slider(f"{tone}", 0, 100, int(st.session_state.tone_scores[tone]))
-        updated_tone_fig = go.Figure(data=[go.Bar(x=list(st.session_state.tone_scores.keys()), y=list(st.session_state.tone_scores.values()))])
-        updated_tone_fig.update_layout(xaxis_title='Tone', yaxis_title='Level')
+            st.session_state.tone_scores[tone] = st.slider(f"{tone}", 0, 10, int(st.session_state.tone_scores[tone]))
+        tone_fig = go.Figure(data=[go.Bar(x=list(st.session_state.tone_scores.keys()), y=list(st.session_state.tone_scores.values()))])
+        tone_fig.update_layout(xaxis_title='Tone', yaxis_title='Level')
         st.subheader("Updated Tone Analysis")
-        st.plotly_chart(updated_tone_fig)
-    if 'sentence_to_colors' in st.session_state and st.session_state.sentence_to_colors:
+        st.plotly_chart(tone_fig)
+    if 'sentence_to_colors' in st.session_state:
         updated_color_counts = Counter()
         for sentence, initial_colors in st.session_state.sentence_to_colors.items():
-            st.write(f"{sentence}: {', '.join(initial_colors)}")
-            selected_colors = st.multiselect(f"Revise colors for: {sentence}.", list(color_keywords.keys()), default=initial_colors)
+            selected_colors = st.multiselect(f"{sentence}.", list(color_keywords.keys()), default=initial_colors)
             for color in selected_colors:
                 updated_color_counts[color] += 1
         updated_fig = draw_donut_chart(updated_color_counts)
         st.subheader('Updated Donut Chart')
         st.plotly_chart(updated_fig)
-        if initial_fig and tone_fig and updated_fig:
-            word_file_path = generate_word_doc(updated_color_counts, user_content, st.session_state.tone_scores, initial_fig, tone_fig, updated_fig)
-            download_link = get_word_file_download_link(word_file_path, "Color_Personality_Analysis_Report.docx")
-            st.markdown(download_link, unsafe_allow_html=True)
+    if initial_fig and tone_fig and updated_fig:
+        word_file_path = generate_word_doc(updated_color_counts, user_content, st.session_state.tone_scores, initial_fig, tone_fig, updated_fig)
+        download_link = get_word_file_download_link(word_file_path, "Color_Personality_Analysis_Report.docx")
+        st.markdown(download_link, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
