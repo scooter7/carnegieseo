29c9e42e73b2f543d72bccf878d4c0561892a561
scooter7 <scooter.vineburgh@gmail.com>
2023-09-10 22:17:50 -0500

    Update carcon.py

--- carcon.py
+++ carcon.py
@@ -23,15 +23,6 @@ def draw_donut_chart(color_counts, color_keywords):
     fig = go.Figure(data=[go.Pie(labels=labels, values=sizes, hole=.3, marker=dict(colors=[colors[label] for label in labels]))])
     return fig
 
-def annotate_sentences_with_color(text, color_keywords):
-    sentences = re.split(r'(?<=[.!?])\s+', text)
-    annotated_sentences = []
-    for sentence in sentences:
-        color_counts = analyze_text(sentence, color_keywords)
-        dominant_color = max(color_counts, key=color_counts.get, default='None')
-        annotated_sentences.append(f"{sentence} ({dominant_color})")
-    return ' '.join(annotated_sentences)
-
 def plot_tone_analysis(tone_scores):
     fig, ax = plt.subplots()
     ax.bar(tone_scores.keys(), tone_scores.values())
@@ -40,6 +31,13 @@ def plot_tone_analysis(tone_scores):
     plt.savefig(buf, format="png")
     buf.seek(0)
     return buf
+
+def get_word_file_download_link(file_path, filename):
+    with open(file_path, "rb") as f:
+        file_data = f.read()
+    b64_file = base64.b64encode(file_data).decode()
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,{b64_file}" download="{filename}">Download Word Report</a>'
+    return href
 
 def generate_word_doc(color_counts, user_content, tone_scores, color_keywords):
     doc = Document()
@@ -57,18 +55,11 @@ def generate_word_doc(color_counts, user_content, tone_scores, color_keywords):
     doc.save(word_file_path)
     return word_file_path
 
-def get_word_file_download_link(file_path, filename):
-    with open(file_path, "rb") as f:
-        file_data = f.read()
-    b64_file = base64.b64encode(file_data).decode()
-    href = f'<a href="data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,{b64_file}" download="{filename}">Download Word Report</a>'
-    return href
-
 def main():
     st.title('Color Personality Analysis')
     if 'user_content' not in st.session_state:
         st.session_state.user_content = ""
-
+        
     color_keywords = {
         'Red': ['Activate', 'Animate', 'Amuse', 'Captivate', 'Cheer', 'Delight', 'Encourage', 'Energize', 'Engage', 'Enjoy', 'Enliven', 'Entertain', 'Excite', 'Express', 'Inspire', 'Joke', 'Motivate', 'Play', 'Stir', 'Uplift', 'Amusing', 'Clever', 'Comedic', 'Dynamic', 'Energetic', 'Engaging', 'Enjoyable', 'Entertaining', 'Enthusiastic', 'Exciting', 'Expressive', 'Extroverted', 'Fun', 'Humorous', 'Interesting', 'Lively', 'Motivational', 'Passionate', 'Playful', 'Spirited'],
         'Silver': ['Activate', 'Campaign', 'Challenge', 'Commit', 'Confront', 'Dare', 'Defy', 'Disrupting', 'Drive', 'Excite', 'Face', 'Ignite', 'Incite', 'Influence', 'Inspire', 'Inspirit', 'Motivate', 'Move', 'Push', 'Rebel', 'Reimagine', 'Revolutionize', 'Rise', 'Spark', 'Stir', 'Fight', 'Free', 'Aggressive', 'Bold', 'Brazen', 'Committed', 'Courageous', 'Daring', 'Disruptive', 'Driven', 'Fearless', 'Free', 'Gutsy', 'Independent', 'Inspired', 'Motivated', 'Rebellious', 'Revolutionary', 'Unafraid', 'Unconventional'],
@@ -80,28 +71,33 @@ def main():
         'Orange': ['Compose', 'Conceptualize', 'Conceive', 'Craft', 'Create', 'Design', 'Dream', 'Envision', 'Express', 'Fashion', 'Form', 'Imagine', 'Interpret', 'Make', 'Originate', 'Paint', 'Perform', 'Portray', 'Realize', 'Shape', 'Abstract', 'Artistic', 'Avant-garde', 'Colorful', 'Conceptual', 'Contemporary', 'Creative', 'Decorative', 'Eccentric', 'Eclectic', 'Evocative', 'Expressive', 'Imaginative', 'Interpretive', 'Offbeat', 'One-of-a-kind', 'Original', 'Uncommon', 'Unconventional', 'Unexpected', 'Unique', 'Vibrant', 'Whimsical'],
         'Pink': ['Arise', 'Aspire', 'Detail', 'Dream', 'Elevate', 'Enchant', 'Enrich', 'Envision', 'Exceed', 'Excel', 'Experience', 'Improve', 'Idealize', 'Imagine', 'Inspire', 'Perfect', 'Poise', 'Polish', 'Prepare', 'Refine', 'Uplift', 'Affectionate', 'Admirable', 'Age-less', 'Beautiful', 'Classic', 'Desirable', 'Detailed', 'Dreamy', 'Elegant', 'Enchanting', 'Enriching', 'Ethereal', 'Excellent', 'Exceptional', 'Experiential', 'Exquisite', 'Glamorous', 'Graceful', 'Idealistic', 'Inspiring', 'Lofty', 'Mysterious', 'Ordered', 'Perfect', 'Poised', 'Polished', 'Pristine', 'Pure', 'Refined', 'Romantic', 'Sophisticated', 'Spiritual', 'Timeless', 'Traditional', 'Virtuous', 'Visionary']
     }
-
+    
+    tone_keywords = {
+        "Relaxed": ["calm", "peaceful", "easygoing", "informal"],
+        "Assertive": ["confident", "aggressive", "self-assured", "dogmatic"],
+        "Introverted": ["calm", "solitude", "introspective", "reserved"],
+        "Extroverted": ["social", "energetic", "outgoing"],
+        "Conservative": ["traditional", "status quo", "orthodox"],
+        "Progressive": ["reform", "liberal", "innovative"],
+        "Emotive": ["emotional", "passionate", "intense"],
+        "Informative": ["inform", "disclose", "instructive"]
+    }
+        
     user_content = st.text_area('Paste your content here:', value=st.session_state.user_content)
     st.session_state.user_content = user_content
-    
+
     if st.button('Analyze'):
-        annotated_text = annotate_sentences_with_color(user_content, color_keywords)
-        st.session_state.user_content = annotated_text
-
-    st.subheader("Revision Field")
-    revision_input = st.text_area("Paste a sentence here for revision:")
-    revised_color = st.selectbox("Select the revised color:", list(color_keywords.keys()))
-    
-    if st.button("Apply Revision"):
-        if revision_input:
-            pattern = re.escape(revision_input.strip()) + r'\s*\((\w+)\)'
-            match = re.search(pattern, st.session_state.user_content)
-            if match:
-                old_color = match.group(1)
-                revised_sentence = f"{revision_input.strip()} ({revised_color})"
-                st.session_state.user_content = re.sub(pattern, revised_sentence, st.session_state.user_content)
-
-    st.text_area('Updated content:', value=st.session_state.user_content)
+        color_counts = analyze_text(user_content, color_keywords)
+        fig = draw_donut_chart(color_counts, color_keywords)
+        st.plotly_chart(fig)
+        
+        tone_scores = analyze_text(user_content, tone_keywords)
+        tone_buf = plot_tone_analysis(tone_scores)
+        st.image(tone_buf, caption='Tone Analysis', use_column_width=True)
+        
+        word_file_path = generate_word_doc(color_counts, user_content, tone_scores, color_keywords)
+        download_link = get_word_file_download_link(word_file_path, "Color_Personality_Analysis_Report.docx")
+        st.markdown(download_link, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
