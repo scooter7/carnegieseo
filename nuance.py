8fe4668e736d1bca7208ed19fe90ac3af2578bf4
scooter7 <scooter.vineburgh@gmail.com>
2023-09-22 14:24:27 -0500

    Update nuance.py

--- nuance.py
+++ nuance.py
@@ -6,28 +6,8 @@ import io
 import base64
 import openai
 
-def analyze_text(text, color_profiles):
-    color_scores = Counter()
-    for color, profile in color_profiles.items():
-        for characteristic in profile['key_characteristics']:
-            color_scores[color] += text.lower().count(characteristic.lower())
-        for tip in profile['messaging_tips']:
-            color_scores[color] += text.lower().count(tip.lower())
-        for tone in profile['tone_and_style']:
-            color_scores[color] += text.lower().count(tone.lower())
-    return color_scores
-
-def draw_donut_chart(color_scores, color_to_hex):
-    labels = list(color_scores.keys())
-    values = [color_scores[color] for color in labels]
-    colors = [color_to_hex[color] for color in labels]
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3, marker=dict(colors=colors))])
-    return fig
-
-def main():
-    st.title('Color Personality Analysis')
-
-    color_profiles = {
+# Define color profiles, color to hex mapping, and color to style mapping here
+color_profiles = {
         'Silver': {'key_characteristics': ['rebellious', 'rule-breaking', 'freedom', 'fearless', 'risks'], 'tone_and_style': ['intriguing', 'expressive', 'focused', 'intentional', 'unbound', 'bold', 'brash'], 'messaging_tips': ['spectrum', 'independence', 'freedom', 'unconventional', 'bold', 'dangerous', 'empower', 'embolden', 'free', 'fearless']},
         'Purple': {'key_characteristics': ['care', 'encourage', 'safe', 'supported', 'help', 'heal'], 'tone_and_style': ['warm', 'gentle', 'accessible', 'relatable', 'personable', 'genuine', 'intimate', 'invitational'], 'messaging_tips': ['personable', 'care', 'compassion', 'friendship', 'deep', 'nurtures', 'protects', 'guides', 'comes alongside']},
         'Pink': {'key_characteristics': ['elegant', 'sophisticated', 'experience', 'excellence', 'beauty', 'vitality'], 'tone_and_style': ['elevated', 'ethereal', 'thoughtful', 'meaningful', 'aspirational', 'dreamy'], 'messaging_tips': ['fine details', 'intentionality', 'unique experiences', 'elevated language', 'excellence', 'refinement', 'inspire', 'uplift', 'desired', 'important']},
@@ -36,8 +16,8 @@ def main():
         'Orange': {'key_characteristics': ['creative', 'original', 'self-expression', 'artistry', 'new ideas', 'modes of expression'], 'tone_and_style': ['exuberant', 'vivid', 'colorful', 'unrestrained', 'abstract', 'unconventional', 'interesting constructs', 'sentence structure'], 'messaging_tips': ['expressive freedom', 'art for art’s sake', 'original', 'creative', 'diversity', 'imagination', 'ideation']},
         'Blue': {'key_characteristics': ['growth', 'industry leader', 'stability', 'pride', 'strength', 'influence', 'accomplishment'], 'tone_and_style': ['bold', 'confident', 'self-assured', 'proud'], 'messaging_tips': ['bold', 'confident', 'self-assured', 'proud', 'powerful']}
     }
-
-    color_to_hex = {
+	
+	color_to_hex = {
         'Silver': '#C0C0C0',
         'Purple': '#800080',
         'Pink': '#FFC0CB',
@@ -49,40 +29,54 @@ def main():
         'Green': '#008000'
     }
 
-    user_content = st.text_area('Paste your content here:')
-    analyze_button = st.button('Analyze')
+def draw_donut_chart(color_counts, color_to_hex):
+    labels = color_counts.keys()
+    values = color_counts.values()
+    colors = [color_to_hex[color] for color in labels]
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3, marker=dict(colors=colors))])
+    return fig
 
-    if analyze_button:
-        color_scores = analyze_text(user_content, color_profiles)
-        initial_fig = draw_donut_chart(color_scores, color_to_hex)
-        st.subheader('Initial Donut Chart')
+def analyze_text(text, color_profiles):
+    color_scores = Counter()
+    for color, profile in color_profiles.items():
+        for characteristic in profile['key_characteristics']:
+            color_scores[color] += text.lower().count(characteristic.lower())
+        for tip in profile['messaging_tips']:
+            color_scores[color] += text.lower().count(tip.lower())
+        for tone in profile['tone_and_style']:
+            color_scores[color] += text.lower().count(tone.lower())
+    return color_scores
+
+def main():
+    st.title('Color Personality Analysis')
+    user_content = st.text_area('Paste your content here:')
+    
+    if 'color_scores' not in st.session_state:
+        st.session_state['color_scores'] = Counter()
+    if 'updated_color_scores' not in st.session_state:
+        st.session_state['updated_color_scores'] = Counter()
+
+    if st.button('Analyze'):
+        st.session_state['color_scores'] = analyze_text(user_content, color_profiles)
+        st.session_state['updated_color_scores'] = st.session_state['color_scores'].copy()
+
+        initial_fig = draw_donut_chart(st.session_state['color_scores'], color_to_hex)
         st.plotly_chart(initial_fig)
 
         sentences = re.split(r'[.!?]', user_content)
-        updated_color_scores = Counter()
-
         for sentence in sentences:
             if sentence.strip():
                 selected_colors = st.multiselect(f"{sentence}.", list(color_profiles.keys()), key=sentence)
-                updated_color_scores.update(selected_colors)
+                st.session_state['updated_color_scores'].update(selected_colors)
 
-        if updated_color_scores:
-            updated_fig = draw_donut_chart(updated_color_scores, color_to_hex)
-            st.subheader('Updated Donut Chart')
-            st.plotly_chart(updated_fig)
+        updated_fig = draw_donut_chart(st.session_state['updated_color_scores'], color_to_hex)
+        st.plotly_chart(updated_fig)
 
-        dominant_color = updated_color_scores.most_common(1)[0][0] if updated_color_scores else None
-        supporting_colors = [color for color, count in updated_color_scores.items() if color != dominant_color]
+        dominant_color = st.session_state['updated_color_scores'].most_common(1)[0][0] if st.session_state['updated_color_scores'] else None
+        supporting_colors = [color for color, count in st.session_state['updated_color_scores'].items() if color != dominant_color]
 
-        if dominant_color:
-            st.write(f"The dominant color in the content appears to be {dominant_color}.")
-        else:
-            st.write(f"No dominant color could be determined from the content.")
-
-        if supporting_colors:
-            st.write(f"The supporting colors are {', '.join(supporting_colors)}. They serve to complement the dominant color by adding layers of complexity to the message.")
-        else:
-            st.write(f"No supporting colors could be determined.")
+        st.write(f"The dominant color in the content appears to be {dominant_color}.")
+        st.write(f"The supporting colors are {', '.join(supporting_colors)}. They serve to complement the dominant color by adding layers of complexity to the message.")
 
 if __name__ == '__main__':
     main()
