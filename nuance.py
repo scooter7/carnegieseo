b95b9340aae907e8124883ac6c8273a098bc3734
scooter7 <scooter.vineburgh@gmail.com>
2023-09-22 22:22:24 -0500

    Update nuance.py

--- nuance.py
+++ nuance.py
@@ -91,14 +91,10 @@ def main():
                 st.write("---")
                 color_count[primary_color] = color_count.get(primary_color, 0) + 1
             
-            if color_count:
-                color_count_df = pd.DataFrame(list(color_count.items()), columns=['Color', 'Count'])
-                color_discrete_map = {color: color_to_hex.get(color, "#FFFFFF") for color in color_count_df['Color']}
-                fig = px.pie(color_count_df, names='Color', values='Count', color='Color', color_discrete_map=color_discrete_map, hole=0.4, width=800, height=400)
-                fig.update_layout(legend_title_text='Color')
-                st.plotly_chart(fig)
-            else:
-                st.error("No primary colors were identified for the given URLs.")
+            color_count_df = pd.DataFrame(list(color_count.items()), columns=['Color', 'Count'])
+            color_discrete_map = {color: color_to_hex.get(color, "#FFFFFF") for color in color_count_df['Color']}
+            fig = px.pie(color_count_df, names='Color', values='Count', color='Color', color_discrete_map=color_discrete_map, hole=0.4, width=800, height=400)
+            st.plotly_chart(fig)
 
 if __name__ == "__main__":
     main()
