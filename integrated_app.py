22cba33145f53939f90867727d2ba9de2d23611b
scooter7 <scooter.vineburgh@gmail.com>
2023-10-26 15:45:24 -0500

    Update integrated_app.py

--- integrated_app.py
+++ integrated_app.py
@@ -25,22 +25,13 @@ def analyze_text(text, color_keywords):
     words = re.findall(r'\b\w+\b', text)
     color_counts = Counter()
     for color, keywords in color_keywords.items():
-        for keyword in keywords['verbs'] + keywords['adjectives']:
+        for keyword in keywords:
             color_counts[color] += words.count(keyword.lower())
     sorted_colors = sorted(color_counts.items(), key=lambda x: x[1], reverse=True)
     return [color for color, _ in sorted_colors[:3]]
 
 color_keywords = {
-    "Purple - caring, encouraging": {"verbs": ["care", "encourage"], "adjectives": ["caring", "encouraging"]},
-    "Green - adventurous, curious": {"verbs": ["explore", "discover"], "adjectives": ["adventurous", "curious"]},
-    "Maroon - gritty, determined": {"verbs": ["persevere", "strive"], "adjectives": ["gritty", "determined"]},
-    "Orange - artistic, creative": {"verbs": ["create", "express"], "adjectives": ["artistic", "creative"]},
-    "Yellow - innovative, intelligent": {"verbs": ["innovate", "intellect"], "adjectives": ["innovative", "intelligent"]},
-    "Red - entertaining, humorous": {"verbs": ["entertain", "amuse"], "adjectives": ["entertaining", "humorous"]},
-    "Blue - confident, influential": {"verbs": ["inspire", "influence"], "adjectives": ["confident", "influential"]},
-    "Pink - charming, elegant": {"verbs": ["charm", "grace"], "adjectives": ["charming", "elegant"]},
-    "Silver - rebellious, daring": {"verbs": ["rebel", "dare"], "adjectives": ["rebellious", "daring"]},
-    "Beige - dedicated, humble": {"verbs": ["dedicate", "humble"], "adjectives": ["dedicated", "humble"]}
+    # ... [Your color dictionary here]
 }
 
 def generate_article(content, writing_styles, style_weights, user_prompt, keywords, audience, specific_facts_stats):
@@ -57,7 +48,7 @@ def generate_article(content, writing_styles, style_weights, user_prompt, keywor
     if writing_styles and style_weights:
         for i, style in enumerate(writing_styles):
             weight = style_weights[i]
-            messages.append({"role": "assistant", "content": f"Modify {weight}% of the content in a {style.split(' - ')[1]} manner."})
+            messages.append({"role": "assistant", "content": f"Modify {weight}% of the content in a {style} manner."})
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
     return response.choices[0].message["content"].strip()
 
@@ -66,6 +57,7 @@ url_input = st.text_area("Paste a list of comma-separated URLs:")
 if st.button("Analyze"):
     urls = [url.strip() for url in url_input.split(",")]
     results = []
+
     for url in urls:
         try:
             content = scrape_content_from_url(url)
@@ -73,6 +65,7 @@ if st.button("Analyze"):
             results.append((url, *top_colors))
         except:
             results.append((url, "Error", "", ""))
+    
     st.session_state.results = results
 
 if 'results' not in st.session_state:
@@ -83,15 +76,12 @@ for idx, (url, color1, color2, color3) in enumerate(st.session_state.results):
         st.write(f"URL: {url}")
         st.write(f"Identified Colors: {color1}, {color2}, {color3}")
         selected_colors = st.multiselect(f"Select new color profiles for {url}:", list(color_keywords.keys()), default=[color1, color2, color3], key=f"color_{idx}")
-        sliders = {}
-        for color in selected_colors:
-            sliders[color] = st.slider(f"Ratio for {color}:", 0, 100, 100 // len(selected_colors), key=f"slider_{color}_{idx}")
         seo_keywords = st.text_input(f"Additional SEO keywords for {url}:", key=f"keywords_{idx}")
         facts = st.text_area(f"Specific facts or stats for {url}:", key=f"facts_{idx}")
         if st.button("Revise", key=f"revise_{idx}"):
             original_content = scrape_content_from_url(url)
-            revised_content = generate_article(original_content, selected_colors, [sliders[color] for color in selected_colors], None, seo_keywords, None, facts)
+            revised_content = generate_article(original_content, selected_colors, [100 // len(selected_colors) for _ in selected_colors], None, seo_keywords, None, facts)
             st.write("Revised Content:")
             st.write(revised_content)
             b64 = base64.b64encode(revised_content.encode()).decode()
-            dl_button = st.download_button(label="Download Revised Content", data=b64, file_name=f'revised_content_{idx}.txt', mime='text/plain')
+            st.download_button(label="Download Revised Content", data=b64, file_name=f'revised_content_{idx}.txt', mime='text/plain')
