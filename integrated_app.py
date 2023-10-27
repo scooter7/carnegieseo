7c22a97d1bfdc176dce7064b5a5710c69e09959e
scooter7 <scooter.vineburgh@gmail.com>
2023-10-26 21:17:43 -0500

    Update integrated_app.py

--- integrated_app.py
+++ integrated_app.py
@@ -5,6 +5,7 @@ from collections import Counter
 import openai
 import base64
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 if "OPENAI_API_KEY" not in st.secrets:
@@ -29,6 +30,27 @@ def analyze_text(text, color_keywords):
     sorted_colors = sorted(color_counts.items(), key=lambda x: x[1], reverse=True)
     return [color for color, _ in sorted_colors[:3]]
 
+def generate_article(content, writing_styles, style_weights, user_prompt, keywords, audience, specific_facts_stats):
+    full_prompt = user_prompt if user_prompt else "Modify the content based on the following guidelines:"
+    if keywords:
+        full_prompt += f"\nInclude these keywords: {keywords}"
+    messages = [{"role": "system", "content": full_prompt}]
+    messages.append({"role": "user", "content": content})
+    if writing_styles and style_weights:
+        for i, style in enumerate(writing_styles):
+            weight = style_weights[i]
+            messages.append({"role": "assistant", "content": f"Modify {weight}% of the content in a {style.split(' - ')[1]} manner."})
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    revised_content = response.choices[0].message["content"].strip()
+    revised_sentences = revised_content.split('. ')
+    vectorizer = TfidfVectorizer().fit(revised_sentences)
+    for fact in specific_facts_stats.split('\n'):
+        fact_vector = vectorizer.transform([fact]).toarray()[0]
+        similarities = cosine_similarity(fact_vector, vectorizer.transform(revised_sentences).toarray())
+        index = np.argmax(similarities)
+        revised_sentences[index] = f"{revised_sentences[index]}. {fact}"
+    return ". ".join(revised_sentences)
+
 color_keywords = {
     "Purple - caring, encouraging": {"verbs": ["care", "encourage"], "adjectives": ["caring", "encouraging"]},
     "Green - adventurous, curious": {"verbs": ["explore", "discover"], "adjectives": ["adventurous", "curious"]},
@@ -41,31 +63,6 @@ color_keywords = {
     "Silver - rebellious, daring": {"verbs": ["rebel", "dare"], "adjectives": ["rebellious", "daring"]},
     "Beige - dedicated, humble": {"verbs": ["dedicate", "humble"], "adjectives": ["dedicated", "humble"]}
 }
-
-def insert_facts_based_on_context(content_sentences, facts):
-    vectorizer = TfidfVectorizer().fit(content_sentences)
-    content_vectors = vectorizer.transform(content_sentences)
-    for fact in facts:
-        fact_vector = vectorizer.transform([fact])
-        cosine_similarities = (content_vectors * fact_vector.T).toarray()
-        best_sentence_index = np.argmax(cosine_similarities)
-        content_sentences[best_sentence_index] += f" {fact}"
-    return ' '.join(content_sentences)
-
-def generate_article(content, writing_styles, style_weights, user_prompt, keywords, audience, specific_facts_stats):
-    full_prompt = user_prompt if user_prompt else "Starting with the original content, modify the article with the following guidelines:"
-    messages = [{"role": "system", "content": full_prompt}]
-    if content:
-        messages.append({"role": "user", "content": content})
-    if writing_styles and style_weights:
-        for i, style in enumerate(writing_styles):
-            weight = style_weights[i]
-            messages.append({"role": "assistant", "content": f"Modify {weight}% of the content in a {style.split(' - ')[1]} manner."})
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-    revised_content = response.choices[0].message["content"].strip()
-    revised_sentences = revised_content.split('. ')
-    revised_content = insert_facts_based_on_context(revised_sentences, specific_facts_stats.split('\n'))
-    return revised_content
 
 url_input = st.text_area("Paste a list of comma-separated URLs:")
 
@@ -95,9 +92,8 @@ for idx, (url, content, color1, color2, color3) in enumerate(st.session_state.re
     seo_keywords = st.text_input(f"Additional SEO keywords for {url}:", key=f"keywords_{idx}")
     facts = st.text_area(f"Specific facts or stats for {url}:", key=f"facts_{idx}")
     if st.button("Revise", key=f"revise_{idx}"):
-        original_content = scrape_text(url)
-        revised_content = generate_article(original_content, selected_colors, [sliders[color] for color in selected_colors], None, seo_keywords, None, facts)
+        revised_content = generate_article(content, selected_colors, [sliders[color] for color in selected_colors], None, seo_keywords, None, facts)
         st.write("Revised Content:")
         st.write(revised_content)
-        b64 = base64.b64encode(revised_content.encode()).decode()
+        b64 = base64.b64encode(revised_content.encode('utf-8')).decode()
         dl_button = st.download_button(label="Download Revised Content", data=b64, file_name=f'revised_content_{idx}.txt', mime='text/plain')
